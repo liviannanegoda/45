cb094e90af95afdde5c943983e7093de054f95a7
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-05-29 11:21:01 +0300

    Create Seminar8_HW_Application.java

---
+++ service/Seminar8_HW_Application.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar8_HW_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Seminar8_HW_Application.class, args);
+    }
+}
